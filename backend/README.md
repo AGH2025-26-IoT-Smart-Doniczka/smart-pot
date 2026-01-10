3e81a2eed990b1152e57be3833c5ff89a7c36cc9
Eliza <elizapetrycka@gmail.com>
2026-01-11 00:14:00 +0100

    Added postgres database: service and init

--- backend/README.md
+++
@@ -1,3 +0,0 @@
-# Backend
-
-Java backend service. Put Spring/Gradle/Maven sources, configs, and tests here.
---
+++ infra/app/init/queries.py
@@ -0,0 +1,37 @@
+queries = [
+    """
+    CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+    """,
+    """
+    CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+    """,
+    """
+    CREATE TABLE IF NOT EXISTS users (
+        user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+        email TEXT UNIQUE NOT NULL,
+        password_hash TEXT NOT NULL,
+        salt TEXT NOT NULL,
+        username TEXT NOT NULL
+    )
+    """,
+    """
+    CREATE TABLE IF NOT EXISTS connections (
+        user_id UUID NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
+        pot_id UUID UNIQUE NOT NULL DEFAULT uuid_generate_v1(),
+        active BOOLEAN DEFAULT TRUE,
+        PRIMARY KEY (user_id, pot_id)
+    )
+    """,
+    """
+    CREATE TABLE IF NOT EXISTS measures (
+        pot_id UUID NOT NULL REFERENCES connections(pot_id) ON DELETE CASCADE,
+        timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        air_temp REAL,
+        air_humidity INTEGER,
+        air_pressure INTEGER,
+        soil_moisture REAL,
+        illuminance INTEGER,
+        PRIMARY KEY (pot_id, timestamp)
+    )
+    """,
+]
