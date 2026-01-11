a1205ae717af8b08ff49b2463320a4a7da761f3b
Eliza <elizapetrycka@gmail.com>
2026-01-11 11:02:49 +0100

    Added logging in

--- infra/app/api/endpoints/auth.py
+++ infra/app/api/endpoints/auth.py
@@ -1,8 +1,32 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
+
+from ..utils.jwt_token import create_access_token
+from ..utils.hash import hash_password
+from ..schemas.auth import LoginRequest
+from ..repositories.auth import verify_user_credentials
 
 
 router = APIRouter()
+    
+@router.post("/login", status_code=status.HTTP_200_OK)
+def login(data: LoginRequest):
+    user, message = verify_user_credentials(data.email, data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail={
+                "message": "Sign-in failed.",
+                "error": message
+            }
+        )
+    token = create_access_token(user)
 
-@router.post("/login")
-def login():
-    return {"status": "logged in"}
+    return {
+        "message": message,
+        "user": {
+            "email": user["email"],
+            "username": user["username"],
+            "id": str(user["user_id"])
+        },
+        "jwt": token
+    }
