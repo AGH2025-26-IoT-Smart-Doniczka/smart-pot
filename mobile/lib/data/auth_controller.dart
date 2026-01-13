692a8f3ed24f9571bddaf9590a9a2125871a73aa
stanislawWojtas <stawojtas@student.agh.edu.pl>
2026-01-13 23:27:36 +0100

    login and registration working

--- mobile/lib/data/auth_controller.dart
+++ mobile/lib/data/auth_controller.dart
@@ -46,6 +46,15 @@ class AuthController extends ChangeNotifier {
     );
   }
 
+  Future<bool> register(String email, String password, String username) async {
+    return _authenticate(endpoint: '/user',
+        body: {
+          'email': email,
+          'password': password,
+          'username': username
+        });
+  }
+
   Future<bool> login(String email, String password) async {
     return _authenticate(
       endpoint: '/auth/login',
